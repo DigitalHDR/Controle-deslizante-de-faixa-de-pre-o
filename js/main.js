2d8733a0c76fe7b99e5c82bdf9df49dfcf32489c
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-01-17 15:03:09 -0300

    final

---
+++ js/main.js
@@ -0,0 +1,42 @@
+const rangeInput = document.querySelectorAll(".range-input input")
+priceInput = document.querySelectorAll(".price-input input")
+progress = document.querySelector(".slider .progress")
+
+let princeGap = 1000
+
+priceInput.forEach(input => {
+  input.addEventListener("input", e => {
+    let minVal = parseInt(priceInput[0].value),
+      maxVal = parseInt(priceInput[1].value)
+
+    if ((maxVal - minVal >= princeGap) && maxVal <= 10000) {
+      if(e.target.className === "input-min") {
+        rangeInput[0].value = minVal
+        progress.style.left = (minVal / rangeInput[0].max) * 100 + "%"
+      } else {
+        rangeInput[1].value = maxVal
+        progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%"
+      }
+    }
+  })
+})
+
+rangeInput.forEach(input => {
+  input.addEventListener("input", e => {
+    let minVal = parseInt(rangeInput[0].value),
+      maxVal = parseInt(rangeInput[1].value)
+
+    if (maxVal - minVal < princeGap) {
+      if(e.target.className === "range-min") {
+        rangeInput[0].value = maxVal - princeGap
+      } else {
+        rangeInput[1].value = minVal + princeGap
+      }
+    } else {
+      priceInput[0].value = minVal
+      priceInput[1].value = maxVal
+      progress.style.left = (minVal / rangeInput[0].max) * 100 + "%"
+      progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%"
+    }
+  })
+})
